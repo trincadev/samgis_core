deb571a14bed2267f2d30afb5677cb1592db351b
ale <alenewmail-4@yahoo.it>
2024-03-17 12:29:23 +0100

    [chore] bump to version 1.0.6, change way to show package version

--- samgis_core/__version__.py
+++ samgis_core/__version__.py
@@ -1,1 +1,7 @@
-__version__ = "1.0.5"
+import importlib.metadata
+
+
+try:
+    __version__ = importlib.metadata.version(__package__ or __name__)
+except Exception:
+    __version__ = importlib_metadata.version(__package__ or __name__)
