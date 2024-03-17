e58e82dece4d3b77a6336c6bfffac881859350fd
alessandro trinca tornidor <alessandro@trinca.tornidor.com>
2024-03-17 13:11:04 +0100

    [fix] use canonical way to show version using importlib.metadata.version (with dot), bump to version 1.0.7

--- samgis_core/__version__.py
+++ samgis_core/__version__.py
@@ -1,7 +1,4 @@
 import importlib.metadata
 
 
-try:
-    __version__ = importlib.metadata.version(__package__ or __name__)
-except Exception:
-    __version__ = importlib_metadata.version(__package__ or __name__)
+__version__ = importlib.metadata.version(__package__ or __name__)
