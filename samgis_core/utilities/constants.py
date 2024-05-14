8a93f9d6f51cfc270fbec91db00e570dcabb6c27
alessandro trinca tornidor <alessandro@trinca.tornidor.com>
2024-05-15 00:38:15 +0200

    [feat] handle MODEL_ENCODER_NAME, MODEL_DECODER_NAME as env variables, default still mobile_sam.encoder.onnx, mobile_sam.decoder.onnx

--- samgis_core/utilities/constants.py
+++ samgis_core/utilities/constants.py
@@ -1,5 +1,6 @@
 """Project constants"""
-DEFAULT_INPUT_SHAPE = 684, 1024
-MODEL_ENCODER_NAME = "mobile_sam.encoder.onnx"
-MODEL_DECODER_NAME = "mobile_sam.decoder.onnx"
+import os
 
+DEFAULT_INPUT_SHAPE = 684, 1024
+MODEL_ENCODER_NAME = os.getenv("MODEL_ENCODER_NAME", "mobile_sam.encoder.onnx")
+MODEL_DECODER_NAME = os.getenv("MODEL_DECODER_NAME", "mobile_sam.decoder.onnx")
