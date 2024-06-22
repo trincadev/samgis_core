50838eac92c7b1a4059cd155e620ef3ca122dbd3
alessandro trinca tornidor <alessandro@trinca.tornidor.com>
2024-06-22 17:13:23 +0200

    [test] refactor some test cases

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,6 +1,7 @@
-from samgis_core import PROJECT_ROOT_FOLDER
-
+from samgis_core import PROJECT_ROOT_FOLDER, setup_logging
 
 TEST_ROOT_FOLDER = PROJECT_ROOT_FOLDER / "tests"
 TEST_EVENTS_FOLDER = TEST_ROOT_FOLDER / "events"
 LOCAL_URL_TILE = "http://localhost:8000/lambda_handler/{z}/{x}/{y}.png"
+
+test_logger = setup_logging(debug=True)
--- tests/prediction_api/test_sam_onnx2_inference.py
+++ tests/prediction_api/test_sam_onnx2_inference.py
@@ -1,5 +1,4 @@
 import json
-import logging
 import unittest
 from unittest.mock import patch
 
@@ -10,8 +9,8 @@ from samgis_core import MODEL_FOLDER
 from samgis_core.prediction_api import sam_onnx_inference, sam_onnx2
 from samgis_core.prediction_api.sam_onnx2 import SegmentAnythingONNX2
 from samgis_core.utilities.constants import MODEL_ENCODER_NAME, MODEL_DECODER_NAME
-from samgis_core.utilities.plot_images import helper_imshow_output_expected
-from tests import TEST_EVENTS_FOLDER
+from . import helper_assertions
+from tests import TEST_EVENTS_FOLDER, test_logger
 
 
 instance_sam_onnx = SegmentAnythingONNX2(
@@ -24,47 +23,6 @@ prompt = [{
     "data": [934, 510],
     "label": 0
 }]
-
-
-def count_sum_difference_less_than(a: np.ndarray, b: np.ndarray, **kwargs) -> tuple[float, float]:
-    diff = np.sum(a == b, **kwargs)
-    absolute_count = a.size - diff
-    relative_count = (a.size - diff) / a.size
-    return relative_count, absolute_count
-
-
-def assert_sum_difference_less_than(a: np.ndarray, b: np.ndarray, rtol=1e-05, **kwargs):
-    """
-    assert than the relative (in percentual) count of different values between arrays a and b are less than given 'rtol'
-    parameter. Right now don't evaluate absolute number of values
-    """
-    relative_count, absolute_count = count_sum_difference_less_than(a, b, **kwargs)
-    try:
-        assert relative_count < rtol
-    except AssertionError as ae:
-        logging.error(f"Mismatched elements: {absolute_count} / {a.size} ({relative_count:.02f})")
-        logging.error(f"Max absolute difference: {rtol*a.size}")
-        logging.error(f"Max relative difference: {rtol}")
-        helper_imshow_output_expected(a, b, "a", "b")
-        raise ae
-
-
-def assert_helper_get_raster_inference_with_embedding_from_dict(
-        embedding_dict_test: dict,
-        n_keys: int,
-        name_key,
-        shape_image_embedding=(1, 256, 64, 64),
-        expected_resized_size=(1024, 684)
-    ):
-    assert len(embedding_dict_test) == n_keys
-    assert name_key in list(embedding_dict_test.keys())
-    embedding_dict_test_value = embedding_dict_test[name_key]
-    image_embedding = embedding_dict_test_value["image_embedding"]
-    assert isinstance(image_embedding, np.ndarray)
-    assert image_embedding.shape == shape_image_embedding
-    assert isinstance(embedding_dict_test_value["original_size"], tuple)
-    assert isinstance(embedding_dict_test_value["resized_size"], tuple)
-    assert embedding_dict_test_value["resized_size"] == expected_resized_size
 
 
 class TestSamOnnx2Inference(unittest.TestCase):
@@ -82,9 +40,10 @@ class TestSamOnnx2Inference(unittest.TestCase):
             embedding_key=name_key,
             embedding_dict=embedding_dict_test
         )
-        mask = np.load(TEST_EVENTS_FOLDER / "samexporter_predict" / "teglio" / "mask.npy")
-        allclose_perc = 0.5
-        assert_sum_difference_less_than(output_mask, mask, rtol=allclose_perc)
+        expected_mask = PIL.Image.open(TEST_EVENTS_FOLDER / "samexporter_predict" / "teglio" / "teglio_1280x960_mask2.png")
+        expected_mask = np.array(expected_mask)
+        allclose_perc = 0.05  # percentage
+        helper_assertions.assert_sum_difference_less_than(output_mask, expected_mask, rtol=allclose_perc)
         assert len_inference_out == 1
 
         assert len(embedding_dict_test) == 1
@@ -104,7 +63,7 @@ class TestSamOnnx2Inference(unittest.TestCase):
             inputs_outputs = json.load(tst_json)
 
             n_keys = 1
-            allclose_perc = 0.5
+            allclose_perc = 0.05
             embedding_dict_test = {}
 
             for k, input_output in inputs_outputs.items():
@@ -112,7 +71,7 @@ class TestSamOnnx2Inference(unittest.TestCase):
                 mask = np.load(TEST_EVENTS_FOLDER / f"{name_fn}" / k / "mask.npy", allow_pickle=False)
                 local_prompt = input_output["input"]["prompt"]
                 model_name = input_output["input"]["model_name"]
-                logging.info(f"img.shape: {img.shape}.")
+                test_logger.info(f"img.shape: {img.shape}.")
 
                 output_mask, len_inference_out = sam_onnx_inference.get_raster_inference_with_embedding_from_dict(
                     img=img,
@@ -122,9 +81,9 @@ class TestSamOnnx2Inference(unittest.TestCase):
                     embedding_key=f"embedding_key_test{n_keys}",
                     embedding_dict=embedding_dict_test
                 )
-                assert_sum_difference_less_than(output_mask, mask, rtol=allclose_perc)
+                helper_assertions.assert_sum_difference_less_than(output_mask, mask, rtol=allclose_perc)
                 assert len_inference_out == input_output["output"]["n_predictions"]
-                assert_helper_get_raster_inference_with_embedding_from_dict(
+                helper_assertions.assert_helper_get_raster_inference_with_embedding_from_dict(
                     embedding_dict_test, n_keys, f"embedding_key_test{n_keys}"
                 )
                 n_keys += 1
@@ -150,11 +109,18 @@ class TestSamOnnx2Inference(unittest.TestCase):
                 model_mocked.predict_masks.return_value = inference_out
                 model_mocked.predict_masks.side_effect = None
                 print(f"k:{k}.")
+                test_logger.info(f"k:{k}.")
                 output_mask, len_inference_out = sam_onnx_inference.get_raster_inference(
                     img=img,
                     prompt=local_prompt,
                     models_instance=model_mocked,
                     model_name=model_name
                 )
-                assert np.array_equal(output_mask, mask)
+                try:
+                    assert np.array_equal(output_mask, mask)
+                except Exception as ex:
+                    print(f"k:{k}, ex::{ex}.")
+                    test_logger.error(f"k:{k}, ex::{ex}.")
+                    allclose_perc = 0.002  # percentage
+                    helper_assertions.assert_sum_difference_less_than(output_mask, mask, rtol=allclose_perc)
                 assert len_inference_out == input_output["output"]["n_predictions"]
